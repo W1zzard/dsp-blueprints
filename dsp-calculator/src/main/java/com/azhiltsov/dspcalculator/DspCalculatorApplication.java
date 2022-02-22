14febecfe135d479deef22ceec53f0a325d5d542
azhiltsov <azhiltsov@gmail.com>
2022-02-23 00:15:08 +0700

    Initial template

---
+++ dsp-calculator/src/main/java/com/azhiltsov/dspcalculator/DspCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.azhiltsov.dspcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DspCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DspCalculatorApplication.class, args);
+	}
+
+}
